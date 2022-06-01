4e3f8fba7ce2dbb982e0abc1ab5569de674f05b3
jumaBrian <njengabrianj@gmail.com>
2022-06-01 13:46:04 +0300

    ft: Add Toast to greet new user

--- app/src/main/java/com/brayo/findeatery/MainActivity.java
+++ app/src/main/java/com/brayo/findeatery/MainActivity.java
@@ -1,14 +1,27 @@
 package com.brayo.findeatery;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private Button mDiscoverEateryButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDiscoverEateryButton = findViewById(R.id.DiscoverEateryButton);
+        mDiscoverEateryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"JAMBO",Toast.LENGTH_LONG).show();
+            }
+        }
+        );
     }
 }
