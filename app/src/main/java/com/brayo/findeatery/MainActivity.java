6f16fff390812a677574c1427ad9e1d54d7ad336
jumaBrian <njengabrianj@gmail.com>
2022-06-01 17:11:23 +0300

    ft: Add second Activity plus data parsing from mainActivity to SecondActivity

--- app/src/main/java/com/brayo/findeatery/MainActivity.java
+++ app/src/main/java/com/brayo/findeatery/MainActivity.java
@@ -1,14 +1,20 @@
 package com.brayo.findeatery;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private Button mDiscoverEateryButton;
+    private EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this,"JAMBO",Toast.LENGTH_LONG).show();
+                String location = mLocationEditText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                intent.putExtra("location", location);
+                startActivity(intent);
             }
         }
         );
---
+++ app/src/main/java/com/brayo/findeatery/RestaurantsActivity.java
@@ -0,0 +1,22 @@
+package com.brayo.findeatery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class RestaurantsActivity extends AppCompatActivity {
+
+    private TextView mLocationTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurants);
+
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+        mLocationTextView.setText("Here are all the restaurants near: " + location);
+    }
+}
